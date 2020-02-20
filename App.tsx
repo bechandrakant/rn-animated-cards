c891a2478e2ec3ff4b4567a2f40d5fb4d55bfcb8
Chandrakant <chandrakant@Chandrakants-MacBook-Air.local>
2020-02-20 20:52:44 +0530

    completed animated cards

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomHeader from './src/CustomHeader';
+import SwipeCards from './src/SwipeCards';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <CustomHeader />
+      <SwipeCards />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
